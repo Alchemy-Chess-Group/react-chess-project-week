697c12d9baa0a5c800e552a3a834da2c896ffc39
James Demiraiakian <james.armen@gmail.com>
2022-02-28 14:44:48 -0800

    creating auth logic/routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AuthForm from './components/AuthForm';
+import Header from './components/Header';
+import Home from './components/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/edit-profile">
+            <AuthForm />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </h1>
+  );
 }
--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
+import { useUser } from '../context/UserContext';
+import { signInUser, signUpUser } from '../services/users';
 
 export default function Auth() {
+  const { setUser } = useUser();
+  const history = useHistory();
+
+  const handleAuth = async (email, password) => {
+    try {
+      if (isSigningUp) {
+        await signUpUser(email, password);
+        history.replace('/edit-profile');
+      } else {
+        const resp = await signInUser(email, password);
+        setUser({ id: resp.id, email: resp.email });
+        history.replace('/');
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return (
     <div>
-      <AuthForm />
+      <AuthForm handleAuth={handleAuth} />
     </div>
   );
 }
