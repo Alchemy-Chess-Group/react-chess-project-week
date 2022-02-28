d0bc06a7d7a54ba9dd914c7dc106ac61e485855e
James Demiraiakian <james.armen@gmail.com>
2022-02-28 15:30:37 -0800

    login/register pages render conditionally based on link clicked to navigate

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,28 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthForm from './components/AuthForm';
 import Header from './components/Header';
 import Home from './components/Home';
+import { UserProvider } from './context/UserContext';
 import Auth from './views/Auth';
 
 export default function App() {
   return (
     <h1>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Auth />
-          </Route>
-          <Route exact path="/register">
-            <Auth />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Auth />
+            </Route>
+            <Route exact path="/register">
+              <Auth isSigningUp={true} />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </UserProvider>
     </h1>
   );
 }
