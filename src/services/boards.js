705998c30c9f6ae58958fa154b0ddea0fd43a67a
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-01 12:08:23 -0800

    game state updates table in supabase

--- src/services/boards.js
+++ src/services/boards.js
@@ -4,6 +4,16 @@ export async function createBoard(currentGameState) {
   const request = await client.from('boards').insert([{ currentGameState }]);
   return parseData(request);
 }
+
+export async function fetchCurrentGame() {
+  const request = await client.from('boards').select('*').single();
+  return parseData(request);
+}
+
 export async function updateBoard(id, currentGameState) {
-  const request = await client.from('board').update([currentGameState]);
+  const request = await client
+    .from('boards')
+    .update([{ currentGameState }])
+    .match({ id });
+  return parseData(request);
 }
