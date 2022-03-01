022e912db3e5b13f42fdc654ed8af7160f4ae8df
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-28 16:25:02 -0800

    game move logic in place

--- src/components/ChessBoard/ChessBoardComponent.jsx
+++ src/components/ChessBoard/ChessBoardComponent.jsx
@@ -1,9 +1,22 @@
+import Chess from 'chess.js';
+import { useState } from 'react';
 import { Chessboard } from 'react-chessboard';
 
 export default function ChessBoard() {
+  const [game, setGame] = useState(new Chess());
+  console.log('game', game.fen());
+  const onDrop = (startingSquare, targetSquare) => {
+    const gameState = { ...game };
+    const move = gameState.move({
+      from: startingSquare,
+      to: targetSquare,
+    });
+    setGame(gameState);
+    return move;
+  };
   return (
     <div>
-      <Chessboard id="BasicBoard" />
+      <Chessboard id="BasicBoard" onPieceDrop={onDrop} position={game.fen()} />
     </div>
   );
 }
