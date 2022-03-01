e67dda95f5bc3d819d5dd27886613730d6e88ff1
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-01 14:18:15 -0800

    coppies code over from main need to merge to dev

---
+++ src/views/GameRoom.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChessBoard from '../components/ChessBoard/ChessBoard';
+
+export default function GameRoom() {
+  return (
+    <>
+      <h3>Game Room</h3>
+      <div>
+        <ChessBoard />
+      </div>
+    </>
+  );
+}
