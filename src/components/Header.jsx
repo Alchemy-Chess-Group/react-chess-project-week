5a460a6c8dce95f851d826b3b7ea2a92e5e43fb6
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-03-01 10:25:06 -0800

    organization, created some components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h2>Once Upawn a Time</h2>
+      {/* //this buton logs out current user, still needs logout function */}
+      <button>logout</button>
+    </div>
+  );
 }
