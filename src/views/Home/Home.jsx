e176e919fe6eb809722e75f1112b217c32c80105
Noah Del Toro Smyth <87677895+NoahDeltoroSmyth@users.noreply.github.com>
2022-03-03 09:02:53 -0800

    Noah (#29)
    * inital dev branch commit
    
    * initial commit
    
    * got a board aand pieces rendering
    
    * getting board to work -- in progress
    
    * is it working
    
    * game move logic in place
    
    * figuerd out hwo to find the board orentation and color oh turn
    
    * trying to push again
    
    * intial commit for day 2"
    
    * gameBoardCurrent state updates supabase table
    
    * WIP:supabase functions
    
    * game state updates table in supabase
    
    * coppies code over from main need to merge to dev
    
    * user profiles render to page
    
    * edit profile works
    
    * i think we figured it out
    
    * made a function and invert the fen string
    
    * user can log out
    
    * button changes piece color
    
    * fixed janky piece moving bug
    
    * need to deploy to test chessApp
    
    * added to Yamal
    
    * got the game working
    
    * user can reset game board
    
    * chat box real time working
    
    * got the live chat working
    
    * some light css and chatbox is functinal
    
    * some css to end the day
    
    Co-authored-by: minoka kakizaki <minoka.kakizaki@gmail.com>

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ProfileProvider } from '../context/ProfileContext';
-import Profile from '../components/Profile';
-import ProfileList from '../components/ProfileList';
-import { useUser } from '../context/UserContext';
+import { ProfileProvider } from '../../context/ProfileContext';
+import Profile from '../../components/Profile';
+import ProfileList from '../../components/ProfileList';
+import { useUser } from '../../context/UserContext';
+import style from './Home.css';
 
 export default function Home() {
   const { user } = useUser();
   return (
-    <div>
+    <div className={style.homePage}>
       <ProfileProvider>
         {user.email ? <Profile /> : <div></div>}
         <Link to="/game-room">Room 13</Link>
